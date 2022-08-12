0594ab53b58feb189f280ddd254a4ab085ba460b
Ozeias Santos <ozeiasbss@gmail.com>
2022-08-12 16:59:48 -0300

    fim de curso

---
+++ Curso java script/aula15/forsimplis.js
@@ -0,0 +1,15 @@
+let valores = [8, 5, 5, 9, 2]
+valores.sort()
+//console.log(valores)
+
+
+
+for(let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+
+
+    // só funciona para matrizes (areis) e objetos!!
+
+}
+
+// num.indexOf(9)   para buscar no numero no vetor
---
+++ Curso java script/aula16/fucao01.js
@@ -0,0 +1,13 @@
+let num = 6
+
+
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar'
+    }
+
+}
+
+console.log(parimpar(num))
